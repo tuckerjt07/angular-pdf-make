513e42bd4766fbf747edabfb2209b403e2b885a7
tuckerjt07 <tuckerjt07@gmail.com>
2016-01-13 13:00:03 -0600

    Initial Checkin

---
+++ main.js
@@ -0,0 +1,7 @@
+require.config({
+    packages: ["angular-pdf-make"],
+    paths: {
+        angularPdfMakeDirective: 'src/angular-pdf-make',
+        customPdfMake: 'src/custom-pdf-make'
+    }
+});
---
+++ src/angular-pdf-make.js
@@ -0,0 +1,29 @@
+(function () {
+
+'use strict';
+
+angular.module('angularPdfMakeApp')
+    .directive('angularPdfMake', ['$sce', function ($sce) {
+        return {
+            templateUrl: 'app/angularPdfMake/angularPdfMake.html',
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                content: '=',
+                styles: '='
+            },
+            templateUrl: '/client/app/angularPdfMake/angularPdfMake.html',
+            link: function (scope, element, attrs) {
+                var callback;
+                callback = function (dataUrl) {
+                    element.find('iframe')[0].src = $sce.trustAsResourceUrl(dataUrl);
+                    scope.$apply();
+                };
+                pdfMake.createPdf().getDataUrl(callback, null, {
+                    content: scope.content,
+                    styles: scope.styles
+                });
+            }
+        };
+    }]);
+}());
